107a76afa267dfcb471cd39f85275a4468701cac
ZhaiSpace <zhaiworks.studio@gmail.com>
2026-02-10 09:04:27 +0800

    Add files via upload

---
+++ ResabaEdenZhairaG/CurrencyConverter.cs
@@ -0,0 +1,17 @@
+using System;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter amount in USD: ");
+        double usd = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        double eur = usd * rate;
+
+        Console.WriteLine("Amount in EUR: " + eur.ToString("F2"));
+    }
+}
